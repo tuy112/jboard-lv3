c72778a14025b66fb77c80571d38b1bc60fdedbd
KimJaeHyeok <master_catch@naver.com>
2023-06-22 11:08:54 +0900

    2023.06.22

--- routes/authCheck.js
+++ routes/authCheck.js
@@ -7,7 +7,7 @@ module.exports = {
       }
     },
     statusUI: function (request, response) {
-      var authStatusUI = '로그인후 사용 가능합니다'
+      let authStatusUI = '로그인후 사용 가능합니다';
       if (this.isOwner(request, response)) {
         authStatusUI = `${request.session.nickname}님 환영합니다 | <a href="/auth/logout">로그아웃</a>`;
       }
--- routes/db.js
+++ routes/db.js
@@ -1,5 +1,5 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
+const mysql = require('mysql');
+const db = mysql.createConnection({
     host: 'express-database.cjksyhudg2is.ap-northeast-2.rds.amazonaws.com',
     user: 'root',
     password: '1234567890',
